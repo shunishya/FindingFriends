baaab203dd5e3177f94be0f2121fcb7384730727
Nitu <shunishya.shrestha@gmail.com>
2014-07-24 12:27:40 +0545

    preference is added

---
+++ FindingFriends/src/com/findingfriends/utils/FindingFriendsPreferences.java
@@ -0,0 +1,40 @@
+package com.findingfriends.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class FindingFriendsPreferences {
+	private SharedPreferences mSharedPreferences;
+	private SharedPreferences.Editor mEditor;
+
+	public static final String PREFS_NAME = "FindingFriendsPreference";
+	public static final String USER_ID = "user_id";
+	public static final String PHONE_NUMBER = "phone_number";
+
+	public FindingFriendsPreferences(Context context) {
+		mSharedPreferences = context.getSharedPreferences(PREFS_NAME,
+				Context.MODE_PRIVATE);
+
+	}
+
+	public void setUserID(String user_id) {
+		mEditor = mSharedPreferences.edit();
+		mEditor.putString(USER_ID, user_id);
+		mEditor.commit();
+	}
+
+	public String getUserID() {
+		return mSharedPreferences.getString(USER_ID, null);
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		mEditor = mSharedPreferences.edit();
+		mEditor.putString(PHONE_NUMBER, phoneNumber);
+		mEditor.commit();
+	}
+
+	public String getPhoneNumber() {
+		return mSharedPreferences.getString(PHONE_NUMBER, null);
+	}
+
+}
