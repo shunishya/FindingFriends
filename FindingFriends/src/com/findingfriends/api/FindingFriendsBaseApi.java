43bd56b8535e2cde7c7475c1d185c042463cb6c9
Nitu <shunishya.shrestha@gmail.com>
2014-07-25 15:01:28 +0545

    Api is modified. need to test

--- FindingFriends/src/com/findingfriends/api/FindingFriendsBaseApi.java
+++ FindingFriends/src/com/findingfriends/api/FindingFriendsBaseApi.java
@@ -17,9 +17,10 @@ public class FindingFriendsBaseApi {
 	HttpClient client = new DefaultHttpClient();
 	String url = "http://192.168.1.106:8080/Finding_Friends_Server/webresources/register";
 
-	public String getData() {
+	public String getData() throws FindingFriendsException {
 		// Prepare a request object
 		HttpGet httpget = new HttpGet(url);
+		InputStream instream;
 
 		// Execute the request
 		HttpResponse response;
@@ -27,30 +28,41 @@ public class FindingFriendsBaseApi {
 		try {
 			response = client.execute(httpget);
 
-			// Get hold of the response entity
-			HttpEntity entity = response.getEntity();
-			// If the response does not enclose an entity, there is no need
-			// to worry about connection release
-
-			if (entity != null) {
-
-				// A Simple JSON Response Read
-				InputStream instream = entity.getContent();
+			int httpStatusCode = response.getStatusLine().getStatusCode();
+			switch (httpStatusCode) {
+			case 200:
+				// Get hold of the response entity
+				HttpEntity entity = response.getEntity();
+				// If the response does not enclose an entity, there is no need
+				// to worry about connection release
+				instream = entity.getContent();
 				result = JsonUtil.writeInputStreamAsString(instream);
 				// now you have the string representation of the HTML request
 				instream.close();
-			}
-			return result;
+				return result;
+			case 400:
+				throw new FindingFriendsException("Bad Request 400");
+			case 401:
+				throw new FindingFriendsException("Authentication Failed 401.");
+			case 500:
+				throw new FindingFriendsException("Internal Server Error 500");
+			default:
 
+				throw new FindingFriendsException("No results :"
+						+ httpStatusCode);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "Error";
+			throw new FindingFriendsException(
+					"Failed to get response from server :" + e.getMessage());
 		}
+
 	}
 
-	public InputStream postData(String data) {
+	public InputStream postData(String data) throws FindingFriendsException {
 		// Prepare a request object
 		HttpPost httpPost = new HttpPost(url);
+		InputStream instream;
 		httpPost.setHeader("Content-Type", "application/json");
 		try {
 			httpPost.setEntity(new StringEntity(data));
@@ -60,26 +72,30 @@ public class FindingFriendsBaseApi {
 
 		// Execute the request
 		HttpResponse response;
-		String result = null;
 		try {
 			response = client.execute(httpPost);
 
-			// Get hold of the response entity
-			HttpEntity entity = response.getEntity();
-			// If the response does not enclose an entity, there is no need
-			// to worry about connection release
-
-			if (entity != null) {
-
-				// A Simple JSON Response Read
-				InputStream instream = entity.getContent();
+			int httpStatusCode = response.getStatusLine().getStatusCode();
+			switch (httpStatusCode) {
+			case 200:
+				// Get hold of the response entity
+				HttpEntity entity = response.getEntity();
+				// If the response does not enclose an entity, there is no need
+				// to worry about connection release
+				instream = entity.getContent();
 				return instream;
+			case 400:
+				throw new FindingFriendsException("Bad Request 400");
+			case 401:
+				throw new FindingFriendsException("Authentication Failed 401.");
+			case 500:
+				throw new FindingFriendsException("Internal Server Error 500");
+			default:
+				throw new FindingFriendsException("No results");
 			}
-			return null;
-
 		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
+			throw new FindingFriendsException(
+					"Failed to get response from server :" + e.getMessage());
 		}
 
 	}
