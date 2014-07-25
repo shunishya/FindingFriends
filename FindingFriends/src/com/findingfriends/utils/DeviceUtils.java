b16e3dad77be4b42457fae5bf7957f952bdb2b18
Nitu <shunishya.shrestha@gmail.com>
2014-07-25 09:05:39 +0545

    added DeviceUtils.class

---
+++ FindingFriends/src/com/findingfriends/utils/DeviceUtils.java
@@ -0,0 +1,70 @@
+package com.findingfriends.utils;
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager;
+
+/**
+ * Utility class for accessing various device features such as device id,
+ * telephone number etc.
+ * 
+ * @author tektak
+ * 
+ */
+public class DeviceUtils {
+
+	/**
+	 * Returns Android Secure ID as a unique device ID
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static String getUniqueDeviceID(Context context) {
+		String identifier = null;
+		TelephonyManager tm = (TelephonyManager) context
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		if (tm != null)
+			identifier = tm.getDeviceId();
+		if (identifier == null || identifier.length() == 0)
+			identifier = Secure.getString(context.getContentResolver(),
+					Secure.ANDROID_ID);
+		// identifier = identifier + Math.round(100);
+		return identifier;
+	}
+
+	/**
+	 * returns the cell phone number from line 1
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static String getTelephoneNumber(Context context) {
+		TelephonyManager tMgr = (TelephonyManager) context
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		return tMgr.getLine1Number();
+	}
+
+	/**
+	 * Gets the country ISO from telephony network
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static String getCountryIso(Context context) {
+		TelephonyManager tMgr = (TelephonyManager) context
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		String country = tMgr.getNetworkCountryIso();
+		if (country != null) {
+			if (!country.isEmpty())
+				return country.toUpperCase(Locale.getDefault());
+		 else {
+			country = Locale.getDefault().getCountry();
+			//return country;
+			return "Np"; //Need to comment out and uncomment the above line of code
+		}
+		}
+		return null;
+	}
+}
